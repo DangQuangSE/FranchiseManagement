980bd9113a5ecdca56967d09c424d845f5efd880
DangQuangSE <quangndse180246@fpt.edu.vn>
2026-01-21 11:41:17 +0700

    Integrate table pagination for production plans
    Added a reusable table pagination script and integrated it into the ProductionPlanQuangNds index view. Refactored ProductionPlanQuangNdsController to use service-based data access and updated create/edit actions for improved maintainability.

--- SP26_PRN222_SE1816_ASM01_SE180246_QuangND/FManagement.MVCWebApp.QuangND/Controllers/ProductionPlanQuangNdsController.cs
+++ SP26_PRN222_SE1816_ASM01_SE180246_QuangND/FManagement.MVCWebApp.QuangND/Controllers/ProductionPlanQuangNdsController.cs
@@ -26,7 +26,7 @@ namespace FManagement.MVCWebApp.QuangND.Controllers
             _productPlanQuangNDService = productPlanQuangNDService;
             _storeOrderItemQuangNDService = storeOrderItemQuangNDService;
         }
-   
+
         // GET: ProductionPlanQuangNds
         public async Task<IActionResult> Index()
         {
@@ -35,7 +35,7 @@ namespace FManagement.MVCWebApp.QuangND.Controllers
             var items = await _productPlanQuangNDService.GetAllAsync();
             return View(items);
         }
-        
+
         // GET: ProductionPlanQuangNds/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -43,7 +43,7 @@ namespace FManagement.MVCWebApp.QuangND.Controllers
             {
                 return NotFound();
             }
-           var productionPlanQuangNd = await _productPlanQuangNDService.GetByIdAysnc(id.Value);
+            var productionPlanQuangNd = await _productPlanQuangNDService.GetByIdAysnc(id.Value);
             if (productionPlanQuangNd == null)
             {
                 return NotFound();
@@ -51,33 +51,39 @@ namespace FManagement.MVCWebApp.QuangND.Controllers
 
             return View(productionPlanQuangNd);
         }
-        /*
+
 
         // GET: ProductionPlanQuangNds/Create
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
-            ViewData["KitchenId"] = new SelectList(_context.CentralKitchens, "KitchenId", "KitchenName");
-            ViewData["StoreOrderItemId"] = new SelectList(_context.StoreOrderItemQuangNds, "OrderItemId", "OrderItemId");
+            var StoreOrderItemQuangNds = await _storeOrderItemQuangNDService.GetAllAsync();
+            ViewData["StoreOrderItemId"] = new SelectList(StoreOrderItemQuangNds, "OrderItemId", "OrderItemId");
+
             return View();
         }
+
 
         // POST: ProductionPlanQuangNds/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("PlanId,StoreOrderItemId,KitchenId,PlanDate,StartTime,EndTime,PlanStatus,PriorityLevel,TotalExpectedQuantity,ActualProducedQuantity,BatchNotes,LastModifiedBy,IsDeleted")] ProductionPlanQuangNd productionPlanQuangNd)
+        public async Task<IActionResult> Create(ProductionPlanQuangNd productionPlanQuangNd)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(productionPlanQuangNd);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                var result = await _productPlanQuangNDService.CreateAsync(productionPlanQuangNd);
+                if (result > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
             }
-            ViewData["KitchenId"] = new SelectList(_context.CentralKitchens, "KitchenId", "KitchenName", productionPlanQuangNd.KitchenId);
-            ViewData["StoreOrderItemId"] = new SelectList(_context.StoreOrderItemQuangNds, "OrderItemId", "OrderItemId", productionPlanQuangNd.StoreOrderItemId);
+            var StoreOrderItemQuangNds = await _storeOrderItemQuangNDService.GetAllAsync();
+            ViewData["StoreOrderItemId"] = new SelectList(StoreOrderItemQuangNds, "OrderItemId", "OrderItemId", productionPlanQuangNd.StoreOrderItemId);
             return View(productionPlanQuangNd);
         }
+
 
         // GET: ProductionPlanQuangNds/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -87,13 +93,15 @@ namespace FManagement.MVCWebApp.QuangND.Controllers
                 return NotFound();
             }
 
-            var productionPlanQuangNd = await _context.ProductionPlanQuangNds.FindAsync(id);
+            //var productionPlanQuangNd = await _context.ProductionPlanQuangNds.FindAsync(id);
+            var productionPlanQuangNd = await _productPlanQuangNDService.GetByIdAysnc(id.Value);
             if (productionPlanQuangNd == null)
             {
                 return NotFound();
             }
-            ViewData["KitchenId"] = new SelectList(_context.CentralKitchens, "KitchenId", "KitchenName", productionPlanQuangNd.KitchenId);
-            ViewData["StoreOrderItemId"] = new SelectList(_context.StoreOrderItemQuangNds, "OrderItemId", "OrderItemId", productionPlanQuangNd.StoreOrderItemId);
+            var StoreOrderItemQuangNds = await _storeOrderItemQuangNDService.GetAllAsync();
+            //ViewData["KitchenId"] = new SelectList(_context.CentralKitchens, "KitchenId", "KitchenName", productionPlanQuangNd.KitchenId);
+            ViewData["StoreOrderItemId"] = new SelectList(StoreOrderItemQuangNds, "OrderItemId", "OrderItemId", productionPlanQuangNd.StoreOrderItemId);
             return View(productionPlanQuangNd);
         }
 
@@ -102,35 +110,31 @@ namespace FManagement.MVCWebApp.QuangND.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("PlanId,StoreOrderItemId,KitchenId,PlanDate,StartTime,EndTime,PlanStatus,PriorityLevel,TotalExpectedQuantity,ActualProducedQuantity,BatchNotes,LastModifiedBy,IsDeleted")] ProductionPlanQuangNd productionPlanQuangNd)
+        public async Task<IActionResult> Edit(ProductionPlanQuangNd productionPlanQuangNd)
         {
-            if (id != productionPlanQuangNd.PlanId)
-            {
-                return NotFound();
-            }
+            //if (id != productionPlanQuangNd.PlanId)
+            //{
+            //    return NotFound();
+            //}
 
             if (ModelState.IsValid)
             {
                 try
                 {
-                    _context.Update(productionPlanQuangNd);
-                    await _context.SaveChangesAsync();
+                    var result = await _productPlanQuangNDService.UpdateAsync(productionPlanQuangNd);
+                    if (result > 0)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (Exception e)
                 {
-                    if (!ProductionPlanQuangNdExists(productionPlanQuangNd.PlanId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    throw new Exception(e.Message);
                 }
-                return RedirectToAction(nameof(Index));
             }
-            ViewData["KitchenId"] = new SelectList(_context.CentralKitchens, "KitchenId", "KitchenName", productionPlanQuangNd.KitchenId);
-            ViewData["StoreOrderItemId"] = new SelectList(_context.StoreOrderItemQuangNds, "OrderItemId", "OrderItemId", productionPlanQuangNd.StoreOrderItemId);
+            var StoreOrderItemQuangNds = await _storeOrderItemQuangNDService.GetAllAsync();
+            // ViewData["KitchenId"] = new SelectList(_context.CentralKitchens, "KitchenId", "KitchenName", productionPlanQuangNd.KitchenId);
+            ViewData["StoreOrderItemId"] = new SelectList(StoreOrderItemQuangNds, "OrderItemId", "OrderItemId", productionPlanQuangNd.StoreOrderItemId);
             return View(productionPlanQuangNd);
         }
 
@@ -142,10 +146,7 @@ namespace FManagement.MVCWebApp.QuangND.Controllers
                 return NotFound();
             }
 
-            var productionPlanQuangNd = await _context.ProductionPlanQuangNds
-                .Include(p => p.Kitchen)
-                .Include(p => p.StoreOrderItem)
-                .FirstOrDefaultAsync(m => m.PlanId == id);
+            var productionPlanQuangNd = await _productPlanQuangNDService.GetByIdAysnc(id.Value);
             if (productionPlanQuangNd == null)
             {
                 return NotFound();
@@ -159,19 +160,17 @@ namespace FManagement.MVCWebApp.QuangND.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var productionPlanQuangNd = await _context.ProductionPlanQuangNds.FindAsync(id);
-            if (productionPlanQuangNd != null)
+            var result = await _productPlanQuangNDService.DeleteAsync(id);
+            if(result == false)
             {
-                _context.ProductionPlanQuangNds.Remove(productionPlanQuangNd);
+                return RedirectToAction(nameof(Delete), new { id = id });   
             }
-
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool ProductionPlanQuangNdExists(int id)
-        {
-            return _context.ProductionPlanQuangNds.Any(e => e.PlanId == id);
-        }*/
+        //private bool ProductionPlanQuangNdExists(int id)
+        //{
+        //    return _context.ProductionPlanQuangNds.Any(e => e.PlanId == id);
+        //}
     }
 }
